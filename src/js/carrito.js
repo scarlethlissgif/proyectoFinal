2e9fa62ede43c7b0a79663a6c9e3a9eb4762c658
fernando1996guitar-alt <fernando1996guitar@gmail.com>
2026-01-29 09:44:33 -0500

    inicio del carrito

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -1,2 +1,64 @@
-import '../css/style.css'
-import 'flowbite'
+import '../css/style.css'; 
+import 'flowbite';
+
+//declaramos constantes
+const lista = document.getElementById("lista-carrito");
+const totalPago= document.getElementById("total-compra");
+const btnLimpiar = document.getElementById("btn-limpiar");
+
+//traer la informacion de la memoria del localStorege
+let libroCarrito = JSON.parse(localStorage.getItem("carrito-libros"))||[];
+
+const actualizarContado = () =>{
+    const contador  = document.getElementById("carrito-contador");
+    //si existe el contador le pongamos la longitud deñ carrito
+    if(contador) 
+        contador.innerText=libroCarrito.length;
+};
+
+const mostrarCarrito= ()=>{
+    lista.innerHTML = "";
+    let total = 0;
+
+    libroCarrito.forEach((libro, index) => {
+        total += parseFloat(libro.precio);
+
+        //CARRITO
+        lista.innerHTML += `
+            <div class="flex justify-between items-center border-b border-slate-700 py-4">
+                <p class="font-bold">${libro.titulo}</p>
+                <div class="flex items-center gap-4">
+                    <span class="text-orange-600 font-bold">$${libro.precio}</span>
+                    <button data-index="${index}" class="btn-eliminar text-red-800 text-xs">Eliminar</button>
+                </div>
+            </div>
+        `;
+    });
+totalPago.innerText=`$${total.toFixed(2)}`;
+actualizarContado();
+;}
+
+lista.addEventListener("click", (e) => {
+    const boton = e.target.closest(".btn-eliminar");
+    if(boton){
+        const index = parseInt(boton.dataset.index);
+        //Eliminar el libro del carrito basandose en la posicion seleccionada
+        libroCarrito.splice(index, 1); //splice es un metodo para eliminar
+        //actualizar los datos
+        localStorage.setItem("carrito-libros", JSON.stringify(libroCarrito));
+        mostrarCarrito();
+    }
+})
+
+btnLimpiar.addEventListener("click", () => {
+    // 1. Vaciar el arreglo en memoria
+    libroCarrito = [];
+
+    // 2. Sincronizar el localStorage (guardarlo vacío)
+    localStorage.setItem("carrito-libros", JSON.stringify(libroCarrito));
+
+    // 3. Volver a pintar la interfaz (mostrará la lista vacía y total $0.00)
+    mostrarCarrito();
+});
+
+mostrarCarrito();
